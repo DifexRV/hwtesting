8c78fa584a8115e7d5a20a76347f27d102a033a7
Roman <roOmkin@gmail.com>
2023-12-10 13:37:17 +0700

    Мастер ветка по ДЗ - Тестирование

---
+++ src/main/java/com/example/hwtesting/hwtesting/HwtestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwtesting.hwtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwtestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwtestingApplication.class, args);
+	}
+
+}
